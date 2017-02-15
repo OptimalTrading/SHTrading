eabe46c9462f8a14b74819c8b9933c33ced3f88f
seohasong <tisutoo@gmail.com>
2017-02-15 21:15:04 +0900

    update system: add .gitignore

--- _update.py
+++ _update.py
@@ -1,4 +1,4 @@
-import _mactowin
+import _secret as sec
 from pywinauto import application, timings
 import time
 import os
@@ -8,13 +8,13 @@ app.start("C:/Kiwoom/KiwoomFlash2/khministarter.exe")
 dlg = timings.WaitUntilPasses(20, 0.5, lambda: app.window_(title="번개 Login"))
 pass_ctrl = dlg.Edit2
 pass_ctrl.SetFocus()
-pass_ctrl.TypeKeys('비밀번호~')
+pass_ctrl.TypeKeys(sec.pswd)
 cert_ctrl = dlg.Edit3
 cert_ctrl.SetFocus()
-cert_ctrl.TypeKeys('공인인증서 비밀번호~')
+cert_ctrl.TypeKeys(sec.cpswd)
 btn_ctrl = dlg.Button0
 btn_ctrl.Click()
 
 #문제의 코드
-time.sleep(50)
-os.system("taskkill /im khmini.exe")
+# time.sleep(50)
+# os.system("taskkill /im khmini.exe")
